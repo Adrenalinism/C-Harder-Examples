245748f47fb2b19eba58791a8d3d45bfe50544b4
Marko Bandzo <marko_bandzo@hotmail.com>
2016-11-09 13:09:06 +1300

    Add files via upload

---
+++ print time.cpp
@@ -0,0 +1,63 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+
+   clrscr();
+
+   int hours,mins,seconds,x;                         
+
+   cout<<"Enter hours=";
+
+   cin>>hours;
+
+   cout<<"\nEnter minutes=";
+
+   cin>>mins;
+
+   cout<<"\nEnter seconds=";
+
+   cin>>seconds;
+
+   if(hours > 24)
+
+    {
+
+     cout<<"Invalid Entery";
+
+   }
+
+   else
+
+   {
+
+       cout<<"\n24 Hours Format\n";
+
+cout<<"Hours  :  Mins  :  Seconds\n"<<"  "<<hours<<"  :     "<<mins<<"   :     "<<seconds<<"\n";
+
+  
+  if(hours > 12)
+
+{
+
+  hours=hours-12;
+
+  cout<<"12 Hours Format\n";
+
+cout<<"Hours  :  Mins  :  Seconds\n"<<"  "<<hours<<"  :     "<<mins<<"   :     "<<seconds;
+
+  }
+
+       else
+
+  {
+
+ cout<<"12 Hours Format\n";
+
+ cout<<"Hours  :  Mins  :  Seconds\n"<<" "<<hours<<": "<<mins<<"   :        "<<seconds;
+
+  }
+
+   }
+
+}                            // end of main
